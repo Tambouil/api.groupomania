dd672fa1ec0c52bf74589656f5575fa28abbee52
Valentin Berceaux <v.berceaux@gmail.com>
2022-08-23 18:54:49 +0200

    chore: install auth session package

--- start/routes.ts
+++ start/routes.ts
@@ -20,8 +20,8 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
+Route.get('/ping', async () => {
+  return { message: 'pong' }
 })
 Route.post('/login', 'AuthController.login')
 Route.post('/register', 'AuthController.register')
